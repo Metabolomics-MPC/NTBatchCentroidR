13e25ee7841aaf69f8064ae1656db9b5da4c9898
michaelwitting <michael.witting@helmholtz-muenchen.de>
2022-12-02 08:58:23 +0100

    updated qc plot to serial to save memory

--- R/02_qcplot.R
+++ R/02_qcplot.R
@@ -16,7 +16,7 @@ qc_plot <- function(output,
   # perform centroiding on each file
   percentile <- bplapply(centroid_files,
                          .qc_plot_single,
-                         BPPARAM = BPPARAM,
+                         BPPARAM = SerialParam(),
                          outdir = paste0(output, "/qc_plots"))
   
   
@@ -34,6 +34,8 @@ qc_plot <- function(output,
                             outdir = NA,
                             percentile = 0.05) {
   
+  gc()
+  
   success <- tryCatch({
     
     # workaround until bplapply loads MSnbase
@@ -49,27 +51,36 @@ qc_plot <- function(output,
     
     # read data and get intensities of all centroids
     ms_centroid_data <- readMSData(file_in,
-                                   cache. = 0L,
+                                   msLevel = 1L,
+                                   centroided = TRUE,
                                    mode = "onDisk")
-    
+
     intensities <- unname(unlist(intensity(ms_centroid_data)))
+    
+    intensities <- intensities[which(intensities > 0)]
     
     # create histogram of intensities
     jpeg(file = file_out,
         width = 500,
         height = 500)
-    
+
     hist(log10(intensities),
          freq = FALSE,
          ylim = c(0, 1.1))
-    
-    abline(v = quantile(log10(intensities), probs = 0.01), col = "red", lty = "dashed")
+
+    abline(v = quantile(log10(intensities),
+                        probs = percentile,
+                        na.rm = TRUE),
+           col = "red",
+           lty = "dashed")
 
     dev.off()
     
     # return value with percentile
-    quantile(intensities, probs = percentile)
+    quantile(intensities,
+             probs = percentile,
+             na.rm = TRUE)
     
-  }, error = function(e) {return(NA_real_)})
+  }, error = function(e) {return(1000)})
   
 }
